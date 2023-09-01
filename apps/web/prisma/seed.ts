319d0d086c9d45b7b7b8997c826ee86b17655b0a
Michel Marinho <michel.marinho04@outlook.com>
2023-09-01 10:39:07 -0400

    refactor: changed to turborepo

--- apps/web/prisma/seed.ts
+++ apps/web/prisma/seed.ts
@@ -31,9 +31,10 @@ async function main() {
   console.log({ user })
 }
 main()
-  .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
     console.error(e)
-    await prisma.$disconnect()
     process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
   })
