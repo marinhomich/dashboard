007ae9cdcc914b21ccb28436a3e5bc708b01f616
Michel Marinho <michel.marinho04@outlook.com>
2023-09-01 16:05:58 -0400

    feat: docker compose

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path")
 
 const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
   experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
     serverActions: true,
   },
-  reactStrictMode: true,
-  output: "standalone"
 }
 
 module.exports = nextConfig
